f442f19c48a08f847bf1972205806726fd407ef1
novaELLIAS <eternalkiri@outlook.com>
2021-04-14 16:04:29 +0800

    cmake

---
+++ cmake-build-debug/CMakeFiles/TetrisConsole.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TetrisConsole.dir/main.cpp.obj"
+  "TetrisConsole.exe"
+  "TetrisConsole.exe.manifest"
+  "TetrisConsole.pdb"
+  "libTetrisConsole.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TetrisConsole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
